90c28e091560d8e9748f139bcdb668f8fd11693d
parknari02 <pnr021118@naver.com>
2024-08-12 18:52:35 +0900

    카드 기본 정보 작성 페이지 제작

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage/MainPage";
+import CardBasicPage from "./pages/CardBasicPage/CardBasicPage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path='/card-basic' element={<CardBasicPage/>}/>
       </Routes>
     </div>
   );
